03c698595f515186a663b504e2dd4746fffee1a5
Omar Nasr <omarnasrempires204@gmail.com>
2022-12-18 07:27:59 +0200

    T_RECORD_HANDLING

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import visualization
 ## Input From User
 starting_address = int(input("Please Enter The Starting Address"),16)
 ProgramOrder = input("Please enter the Program Loading Order seperated by commas").strip().split(",")
-print(starting_address)
-print(ProgramOrder)
 
 # Calculate where the Table Starts and Ends Then Create the Memory Table
 start = starting_address-starting_address%16
 end = int("4140",16)
 memory_table = visualization.generate_memory_table(start,end) 
 memory_table.at["4130","5"]="9"
+STAB_LIST = parsing.Create_External_Symbol_Table("HDRTME.txt",ProgramOrder,starting_address)
+memory_table = parsing.Apply_T_RECORDS("HDRTME.txt",STAB_LIST,ProgramOrder,memory_table)
+# print(STAB_LIST)
 print(memory_table)
-print(parsing.Create_External_Symbol_Table("HDRTME.txt",ProgramOrder,starting_address))
-
     
--- parsing.py
+++ parsing.py
@@ -1,3 +1,4 @@
+import pandas as pd
 def Create_External_Symbol_Table(HTERECORDPATH,PROGORDER,STARTING_ADDRESS):
     STAB_LIST = [{} for i in range(len(PROGORDER))]
     with open(HTERECORDPATH) as f:
@@ -7,15 +8,13 @@ def Create_External_Symbol_Table(HTERECORDPATH,PROGORDER,STARTING_ADDRESS):
             if(line[0]=="H"):
                 prog_name_end = line.find("0");
                 PROG_NAME = line[1:prog_name_end].upper().replace("X","")
-                print(PROG_NAME)
                 PROG_START = line[prog_name_end:prog_name_end+6]
-                print(PROG_START)
                 PROG_SIZE = line[prog_name_end+6:prog_name_end+12]
                 LOCAL_SYMTAB[PROG_NAME]=PROG_START
                 LOCAL_SYMTAB["LENGTH"]=PROG_SIZE
             if(line[0]=="D"):
-                final_index = 0
-                while(final_index<len(line)):
+                final_index = 1
+                while(final_index<len(line)-1):
                     prev_index = final_index
                     final_index+=line[final_index:].find("0")
                     if(final_index==-1):
@@ -25,10 +24,9 @@ def Create_External_Symbol_Table(HTERECORDPATH,PROGORDER,STARTING_ADDRESS):
                     LOCAL_SYMTAB[label]=value
                     final_index+=6
 
-                
-                
+
+
             if(line[0]=="E"):
-                print(PROG_NAME)
                 prog_index = PROGORDER.index(PROG_NAME)
                 STAB_LIST[prog_index] = LOCAL_SYMTAB
                 LOCAL_SYMTAB = {}
@@ -40,13 +38,44 @@ def Create_External_Symbol_Table(HTERECORDPATH,PROGORDER,STARTING_ADDRESS):
 
         if(len(STAB.keys())!=0):
             STARTING_ADDRESS = STARTING_ADDRESS+int(STAB["LENGTH"],16)
-        
-
-                
 
     return STAB_LIST
+def Apply_T_RECORDS(HTERECORDPATH,STAB_LIST,PROGORDER,MEMORY_TABLE):
+    with open(HTERECORDPATH) as f:
+        PROG_STARTING_ADDRESS=0
+        for line in f:
+            if(line[0]=="H"): 
+                prog_name_end = line.find("0")
+                PROG_NAME = line[1:prog_name_end].upper().replace("X","")
+                PROG_INDEX = PROGORDER.index(PROG_NAME)
+                PROG_STARTING_ADDRESS = int(STAB_LIST[PROG_INDEX][PROG_NAME],16)
                 
-            
+            if(line[0]=="T"):
+                T_RELATIVE_STARTING_ADDRESS = int(line[1:7],16)
+                T_ABSOLUTE_STARTING_ADDRESS = T_RELATIVE_STARTING_ADDRESS+PROG_STARTING_ADDRESS
+                T_RECORD = line[9:].replace("\n","")
+                CURRENT_ADDRESS = T_ABSOLUTE_STARTING_ADDRESS
+                for i in range(0,len(T_RECORD),2):
+                    byte = T_RECORD[i:i+2]
+                    offset = CURRENT_ADDRESS%16
+                    column = hex(offset)[2:].upper()
+                    row = hex(CURRENT_ADDRESS-offset)[2:].upper()
+                    MEMORY_TABLE.at[row,column]=byte
+                    CURRENT_ADDRESS+=1
+        return MEMORY_TABLE
+
+                    
+
+                
 
 
-    
+
+
+
+
+
+
+
+
+
+
